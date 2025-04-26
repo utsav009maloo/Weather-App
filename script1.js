6aeb4a9569c1a7484b09af81ba1342eb9a7904e0
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-04-26 12:38:39 +0530

    Get location from USER

--- script1.js
+++ script1.js
@@ -1,7 +1,16 @@
 const button = document.getElementById('location-button')
 
-function getlocation(position){
-    console.log(position)
+
+async function GetData(lat, long) {
+    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=5e34fcc8b8c640e3b4c54103252304&q=${lat},${long}&aqi=yes`)
+    return await promise.json()
+
+}
+
+async function getlocation(position){
+    // console.log(position)
+    const result = await GetData(position.coords.latitude,position.coords.longitude)
+    console.log(result)
 }
 
 function failed(){
@@ -11,3 +20,4 @@ function failed(){
 button.addEventListener('click' , async () => {
     const result = navigator.geolocation.getCurrentPosition(getlocation,failed)
 })
+
